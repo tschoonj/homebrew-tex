5207310e6a28ccec0adc1822b16895ce9897c8f5
Alex Dunn <dunn.alex@gmail.com>
2015-07-23 01:22:36 -0700

    auctex: cleanup/modernize
    Closes #12.

--- Formula/auctex.rb
+++ Formula/auctex.rb
@@ -1,34 +1,27 @@
 class Auctex < Formula
+  desc "Emacs package for writing and formatting TeX"
   homepage "https://www.gnu.org/software/auctex/"
   url "http://ftpmirror.gnu.org/auctex/auctex-11.88.tar.gz"
   mirror "https://ftp.gnu.org/gnu/auctex/auctex-11.88.tar.gz"
   sha256 "716867d5fbcc5c67cca781d7c1984e6a3a6d5da056ec3b4f35170805bf4dc83f"
 
   head do
-    url "git://git.savannah.gnu.org/auctex.git"
+    url "http://git.savannah.gnu.org/cgit/auctex.git"
     depends_on "autoconf" => :build
   end
 
   depends_on :tex
-
-  option "with-emacs=", "Path to an emacs binary"
-
-  def which_emacs
-    emacs = ARGV.value("with-emacs") || which("emacs").to_s
-    fail "#{emacs} not found" unless File.exist? emacs
-    emacs
-  end
+  depends_on :emacs => "21.1"
 
   def install
     # configure fails if the texmf dir is not there yet
-    brew_texmf = share/"texmf"
-    brew_texmf.mkpath
+    (share/"texmf").mkpath
 
     system "./autogen.sh" if build.head?
 
     system "./configure", "--prefix=#{prefix}",
-                          "--with-texmf-dir=#{brew_texmf}",
-                          "--with-emacs=#{which_emacs}",
+                          "--with-texmf-dir=#{share}/texmf",
+                          "--with-emacs=#{which "emacs"}",
                           "--with-lispdir=#{share}/emacs/site-lisp"
 
     system "make"
@@ -37,32 +30,15 @@ class Auctex < Formula
   end
 
   def caveats
-    # check if the used emacs is in HOMEBREW_PREFIX/bin
-    # for which case HOMEBREW_PREFIX/share/emacs/site-lisp should already
-    # be by default in the load-path
-    if which_emacs.index("#{HOMEBREW_PREFIX}/bin")
-      dot_emacs = <<-EOS
-      (require 'tex-site)
-      EOS
-    else
-      dot_emacs = <<-EOS
-      (add-to-list 'load-path "#{HOMEBREW_PREFIX}/share/emacs/site-lisp")
-      (require 'tex-site)
-      EOS
-    end
-
     <<-EOS.undent
-    texmf files installed into:
-      #{HOMEBREW_PREFIX}/share/texmf/
+    texmf files have been installed into:
+      #{HOMEBREW_PREFIX}/share/texmf
 
     You can add it to your TEXMFHOME using:
       sudo tlmgr conf texmf TEXMFHOME "~/Library/texmf:#{HOMEBREW_PREFIX}/share/texmf"
 
-    Emacs package installed into:
+    Emacs Lisp files have been installed into:
       #{HOMEBREW_PREFIX}/share/emacs/site-lisp
-
-    To activate, add the following to your .emacs:
-#{dot_emacs}
     EOS
   end
 
