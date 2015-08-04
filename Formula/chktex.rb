aa04181960dad2b3ccce32007566809e57c35384
Alex Dunn <dunn.alex@gmail.com>
2015-08-03 20:39:43 -0700

    chktex 1.7.4
    Closes #15.

--- Formula/chktex.rb
+++ Formula/chktex.rb
@@ -1,11 +1,20 @@
 class Chktex < Formula
+  desc "LaTeX semantic checker"
   homepage "http://www.nongnu.org/chktex/"
-  url "http://download.savannah.gnu.org/releases/chktex/chktex-1.7.2.tar.gz"
-  sha1 "85d9e9fbf3e89104966bd725f156c826d0b44dd9"
+  url "http://download.savannah.gnu.org/releases/chktex/chktex-1.7.4.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/c/chktex/chktex_1.7.4.orig.tar.gz"
+  sha256 "77ed995eabe7088dacf53761933da23e6bf8be14d461f364bd06e090978bf6d2"
+
+  head do
+    url "svn://svn.sv.gnu.org/chktex/trunk/chktex"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
 
   depends_on :tex
 
   def install
+    system "./autogen.sh" if build.head?
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
