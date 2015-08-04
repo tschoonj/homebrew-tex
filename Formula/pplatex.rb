2e6cf90287a40b79e35f7e7f9a4470e76db0b2f1
Alex Dunn <dunn.alex@gmail.com>
2015-08-03 21:10:05 -0700

    pplatex 1.0-rc2
    Closes #16.

--- Formula/pplatex.rb
+++ Formula/pplatex.rb
@@ -1,7 +1,18 @@
 class Pplatex < Formula
-  homepage "http://www.stefant.org/web/projects/software/pplatex.html"
-  url "https://dl.dropbox.com/u/12697903/pplatex/pplatex-1.0-rc1-src.tar.gz"
-  sha256 "27f7ff0fc38bfc4b46325bb15b17278ed09a65a2bf8c76a660a168f0a9006f11"
+  desc "Pretty-printer for LaTeX log output"
+  homepage "https://github.com/stefanhepp/pplatex"
+  head "https://github.com/stefanhepp/pplatex.git"
+
+  stable do
+    url "https://github.com/stefanhepp/pplatex/archive/pplatex-1.0-rc2.tar.gz"
+    sha256 "7bfab2b5a89f838e79408f57349d6eb5c561d4fc51d416c94fe17a219bac97e4"
+
+    # typo in SConstruct; remove with next stable release
+    patch do
+      url "https://github.com/stefanhepp/pplatex/commit/e1f4abbd5f8242e5a4e2cc2667927d81d0326950.diff"
+      sha256 "37453259f8f36845ddfc262d17f3e0d27f2079670d861c692dfc3531fb959764"
+    end
+  end
 
   depends_on "scons" => :build
   depends_on "pcre"
