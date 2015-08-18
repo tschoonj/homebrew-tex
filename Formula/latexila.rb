7b7b247735ad3fc1de44d015229e397897062bc9
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-08-18 20:01:56 +0200

    latexila 3.16.2

--- Formula/latexila.rb
+++ Formula/latexila.rb
@@ -1,8 +1,8 @@
 class Latexila < Formula
-  desc "a LaTeX editor for the GNOME desktop"
+  desc "LaTeX editor for the GNOME desktop"
   homepage "https://wiki.gnome.org/Apps/LaTeXila"
-  url "https://download.gnome.org/sources/latexila/3.16/latexila-3.16.1.tar.xz"
-  sha256 "ab2d769546e10c86670e86271024c023c0a87f1b72856955c1883416a9f333f0"
+  url "https://download.gnome.org/sources/latexila/3.16/latexila-3.16.2.tar.xz"
+  sha256 "5ad20a2acdb8c93dea5aa175a219e62efe5f4a7ffb67bc52ff6faf0c6f42bad3"
 
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
@@ -13,22 +13,27 @@ class Latexila < Formula
   depends_on "libgee"
   depends_on "gobject-introspection"
   depends_on "gnome-icon-theme"
+  depends_on "gnome-themes-standard" => :optional
   depends_on "libxml2" => "with-python"
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on :tex => :recommended
 
   def install
+    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
     system "./configure", "--disable-schemas-compile",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
     system "make", "install"
   end
 
   def post_install
     system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
     system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+    # HighContrast is provided by gnome-themes-standard
+    if File.file?("#{HOMEBREW_PREFIX}/share/icons/HighContrast/.icon-theme.cache")
+      system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/HighContrast"
+    end
   end
 
   test do
