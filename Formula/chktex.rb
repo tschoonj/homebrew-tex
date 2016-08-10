ddaec8e8651ee9735bc71c4511c471e7d114b4ec
Miguel Araújo <miguelarauj1o@gmail.com>
2016-08-10 17:44:57 -0300

    chktex: update link to https (#28)

--- Formula/chktex.rb
+++ Formula/chktex.rb
@@ -1,7 +1,7 @@
 class Chktex < Formula
   desc "LaTeX semantic checker"
   homepage "http://www.nongnu.org/chktex/"
-  url "http://download.savannah.gnu.org/releases/chktex/chktex-1.7.4.tar.gz"
+  url "https://download.savannah.gnu.org/releases/chktex/chktex-1.7.4.tar.gz"
   mirror "https://mirrors.kernel.org/debian/pool/main/c/chktex/chktex_1.7.4.orig.tar.gz"
   sha256 "77ed995eabe7088dacf53761933da23e6bf8be14d461f364bd06e090978bf6d2"
 
